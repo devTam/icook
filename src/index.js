676620ad5e246968720d94b45753240fda663307
Teebreezy101 <tammy.batubo@gmail.com>
2021-01-12 13:50:24 +0100

    add service worker and image files for pwa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Recipe from './components/recipe/Recipe';
 import Navigation from './components/navigation/Navigation';
@@ -20,3 +21,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
